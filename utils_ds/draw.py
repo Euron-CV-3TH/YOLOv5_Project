48276ee3c4b9781cd2a115304710682e2f94c2f9
YeoJin Song <97579755+YeoJins@users.noreply.github.com>
2023-02-12 23:11:41 +0900

    Update draw.py
    bbox+label 삭제, 모자이크 기능 추가

--- utils_ds/draw.py
+++ utils_ds/draw.py
@@ -24,9 +24,13 @@ def draw_boxes(img, bbox, identities=None, offset=(0,0)):
         color = compute_color_for_labels(id)
         label = '{}{:d}'.format("", id)
         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
-        cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
-        cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
-        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
+        #cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
+        w1 = x2 - x1
+        h1 = y2 - y1
+        region = cv2.resize(img[y1:y1+h1,x1:x1+w1], None, fx=0.1, fy=0.1, interpolation=cv2.INTER_AREA)
+        img[y1:y1+h1,x1:x1+w1] = cv2.resize(region, (w1, h1), interpolation=cv2.INTER_AREA)
+        #cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
+        #cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
 
     return img
 
